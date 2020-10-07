bf32ac5c361aebbb156f0c72ce12ca5f47f4b469
Pankaj Patel <pankaj.patel@billie.io>
2020-10-07 23:22:35 +0200

    ⏳ Better Loader

---
+++ src/components/Loading.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoadingContainer = styled.div`
+  min-height: 200px;
+  display: flex;
+  direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Loading = ({ error }: { error: any }) => {
+  if (error) {
+    return null;
+  }
+
+  return (
+    <LoadingContainer>
+      <svg width="120" height="10" viewBox="0 0 120 30" fill="#fff">
+        <circle cx="15" cy="15" r="15">
+          <animate
+            attributeName="r"
+            from="15"
+            to="15"
+            begin="0s"
+            dur="0.8s"
+            values="15;9;15"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="fill-opacity"
+            from="1"
+            to="1"
+            begin="0s"
+            dur="0.8s"
+            values="1;.5;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+          <animate
+            attributeName="r"
+            from="9"
+            to="9"
+            begin="0s"
+            dur="0.8s"
+            values="9;15;9"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="fill-opacity"
+            from="0.5"
+            to="0.5"
+            begin="0s"
+            dur="0.8s"
+            values=".5;1;.5"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="105" cy="15" r="15">
+          <animate
+            attributeName="r"
+            from="15"
+            to="15"
+            begin="0s"
+            dur="0.8s"
+            values="15;9;15"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="fill-opacity"
+            from="1"
+            to="1"
+            begin="0s"
+            dur="0.8s"
+            values="1;.5;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </svg>
+    </LoadingContainer>
+  );
+};
+
+export default Loading;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Loadable from "react-loadable";
 import ScrollAnim from "rc-scroll-anim";
+
+import Loading from "../components/Loading";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Intro from "../components/Intro";
-
-const Loading = ({ error }) => {
-  if (error) {
-    return "Oh nooess!";
-  } else {
-    return <h3>Loading...</h3>;
-  }
-};
 
 const Work = Loadable({
   loading: Loading,
