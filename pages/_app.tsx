4305f886e36523e04b9a27140b038dedc1ff9986
Pankaj Patel <pankaj.patel@abtasty.com>
2025-01-25 06:36:44 +0100

    remove plausible and update next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,9 @@
 import type { AppProps } from "next/app";
-import PlausibleProvider from "next-plausible";
+
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <PlausibleProvider
-    domain={"pankaj.pro"}
-    customDomain={"https://analytics.t2h.app"}
-    trackOutboundLinks={true}
-    selfHosted={true}
-    enabled={true} // TODO: Add NODE_ENV
-    scriptProps={{
-      src: "https://analytics.t2h.app/js/plausible.js",
-      async: true,
-      defer: true,
-    }}
-  >
-    <Component {...pageProps} />
-  </PlausibleProvider>
+  <Component {...pageProps} />
 );
 
 export default App;
