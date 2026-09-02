d5de5f7803b35fe4dd8c668c8d99edbc4bdf9cdf
Pankaj Patel <patel.pankaj@live.in>
2026-09-02 14:24:44 +0200

    chore: update portfolio dependencies and work history

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -2,17 +2,6 @@ import { NextResponse } from 'next/server';
 import formData from 'form-data';
 import Mailgun from 'mailgun.js';
 
-const mailgun = new Mailgun(formData);
-
-const api_key = process.env.MAILGUN_KEY || '';
-const domain = process.env.MAILGUN_DOMAIN || 'mg.pankaj.pro';
-
-const mg = mailgun.client({
-  username: 'api',
-  key: api_key,
-  url: 'https://api.eu.mailgun.net'
-});
-
 interface EmailRequestBody {
   bcc: string;
   [key: string]: any;
@@ -21,6 +10,13 @@ interface EmailRequestBody {
 export async function POST(request: Request) {
   try {
     const reqBody: EmailRequestBody = await request.json();
+    const domain = process.env.MAILGUN_DOMAIN || 'mg.pankaj.pro';
+    const mailgun = new Mailgun(formData);
+    const mg = mailgun.client({
+      username: 'api',
+      key: process.env.MAILGUN_KEY || '',
+      url: 'https://api.eu.mailgun.net'
+    });
 
     const data = {
       from: reqBody.bcc,
